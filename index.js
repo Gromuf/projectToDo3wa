3e6f488490980d34436ab9911444d58518d800c6
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-03-25 11:41:40 +0100

    add and remove

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const todolist = []
+const listContainer = document.getElementById("listContainer")
+const todo = document.getElementById("todoInput")
 function addTodo() {
-    const todo = document.getElementById("todoInput").value
-    todolist.push(todo)
-    displayList()
+    if (todo.value) {
+        const li = document.createElement("li")
+        li.innerHTML = todo.value
+        listContainer.appendChild(li)
+        const span = document.createElement("span")
+        span.innerHTML = "x"
+        li.appendChild(span)
+    }
+    todo.value = ""
 }
 
-function displayList() {
-    const listContainer = document.getElementById("todolist")
-    const ulElement = document.createElement("ul")
-    todolist.forEach((element) => {
-        const liElement = document.createElement("li")
-        liElement.textContent = element
-        ulElement.appendChild(liElement)
-    })
 
-    listContainer.innerHTML = `${ulElement.outerHTML}`
-}
 
 document.getElementById("todoInput").addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         addTodo()
     }
 })
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+    }
+}, false)
